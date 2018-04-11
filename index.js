6e58db51e0eba100ab8f8975d283b154feea760b
Seb Gertz <sebgertz@gmail.com>
2018-04-11 20:20:47 +0000

    revised to use const instead of let

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 let newID = oldID + 10 ** 9;
 
-let ageIsValid = Number.isInteger(currentAge);
+const ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.floor(Math.random() * 20);
+const randomNumber = Math.random() * 20;
 
-let randomInteger = Math.floor(randomNumber);
+const randomInteger = Math.floor(randomNumber);
 
-let randomUserID = randomInteger + 10 ** 9;
+const randomUserID = randomInteger + 10 ** 9;
